2c5d035af5dee8e635475b887b889422b5a6a1fe
Sayantan Dey <sayantandey23@gmail.com>
2020-03-30 14:03:05 +0530

    Updates

---
+++ precache-manifest.c263c56ce16f708c779f27a5f3a25361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf1820b3242b016fe633b7c1aeaf440",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "36d6bf1c9f3fffb1c6c7",
+    "url": "/seasons/static/css/main.236b23d4.chunk.css"
+  },
+  {
+    "revision": "ea0318554fe94600bd9e",
+    "url": "/seasons/static/js/2.719aa7a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.719aa7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d6bf1c9f3fffb1c6c7",
+    "url": "/seasons/static/js/main.635abd9e.chunk.js"
+  },
+  {
+    "revision": "ec9553e90949d1920a53",
+    "url": "/seasons/static/js/runtime-main.9684aab7.js"
+  }
+]);
